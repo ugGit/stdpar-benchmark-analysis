60c9bedc140d08f1e20c01eea10bcc92442d09e2
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2022-08-12 13:29:12 -0700

    add environment information to preprocessing (traccc/isolated)

--- preprocessor.py
+++ preprocessor.py
@@ -15,11 +15,22 @@ def transform_dataframe(df_input):
   df['algorithm'] = df['algorithm'].map(lambda row : re.split('/', row)[1])
 
   # Update the benchmark column to remove the json
-  file_ending_offset = -5
+  file_ending_offset = -len(".json")
   df['benchmark'] = df['benchmark'].map(lambda row : row[:file_ending_offset])
 
+  # Split information for environment from benchmark to individual column
+  benchmarks_in_isolated_environment = df.query("benchmark.str.contains('isolated_env')")['benchmark'].unique()
+  mask = df['benchmark'].isin(benchmarks_in_isolated_environment)
+  df['environment'] = 'traccc'
+  df.loc[mask, 'environment'] = 'isolated'
+  
+  # Remove the trailling caracters from isolated benchmark names
+  prefix_offset = len('stdpar_')
+  sufix_offset = -len('_isolated_env')
+  df.loc[mask, 'benchmark'] = df.loc[mask, 'benchmark'].map(lambda row : row[prefix_offset:sufix_offset])
+
   # Update the dataset column to only include the mu value
-  mu_value_offset = 6
+  mu_value_offset = len('tt_bar')
   df['dataset'] = df['dataset'].map(lambda row : re.split('/', row)[-2][mu_value_offset:])
 
   # Augment dataset with number of activation within a dataset (spread across 10 events)
@@ -35,6 +46,6 @@ def transform_dataframe(df_input):
   df = df.join(df_tait.set_index('dataset'), on='dataset')
 
   # Reorder columns in dataframe
-  df = df[['benchmark', 'algorithm', 'dataset', 'activations', 'kernel_time', 'cpu_time', 'time_unit', 'iterations', 'repetitions']]
+  df = df[['benchmark', 'environment', 'algorithm', 'dataset', 'activations', 'kernel_time', 'cpu_time', 'time_unit', 'iterations', 'repetitions']]
 
   return df
